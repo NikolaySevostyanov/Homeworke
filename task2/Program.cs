31405dc507d4f345fbea835c77d637fbf05c7fc1
Nikolay_S <sandaynsk@yandex.ru>
2022-11-13 19:52:30 +0700

    Выполнил ДЗ

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿int a, b;
+ 
+    Console.WriteLine("Введите два числа : ");
+ 
+    a = Convert.ToInt32(Console.ReadLine());
+ 
+    b = Convert.ToInt32(Console.ReadLine());
+ 
+    if (a > b)
+ 
+    {
+ 
+        Console.Write("наибольшее число ");
+        Console.WriteLine(a);
+        
+    }
+ 
+    else
+ 
+    {
+ 
+         Console.Write("наибольшее число ");
+         Console.WriteLine(b);
+    }
+    
