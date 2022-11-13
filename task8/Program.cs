69a22163fbdc4699369d387efc0216853e4c7c5b
Nikolay_S <sandaynsk@yandex.ru>
2022-11-13 19:48:21 +0700

    Выполнил ДЗ

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;           
+while (i <= N)
+{
+	if (i % 2 == 0)
+	{
+		Console.Write(i + " ");
+		
+	}
+	
+	i++;
+}
