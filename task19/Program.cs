bc2d712224189ac0e285b1b90172b3c7c8ad2f66
Nikolay_S <sandaynsk@yandex.ru>
2022-11-24 20:48:44 +0700

    Выполнение домашнего задания к четвертой лекции по C#

---
+++ task19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Palindrome()
+{
+    int num1 = number / 10000;
+    int num2 = number / 1000 % 10;
+    int num4 = number % 100 / 10;
+    int num5 = number % 10;
+
+    return(num1 == num5 && num2 == num4);
+}
+
+Console.WriteLine(Palindrome() ? "Палиндром" : "Не является палиндромомо");
---
+++ task21/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координату точки xa: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки ya: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки za: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки xb: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки yb: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату точки zb: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double xc = (xb - xa) * (xb - xa);
+    double yc = (yb - ya) * (yb - ya);
+    double zc = (zb - za) * (zb - za);
+    double result = Math.Sqrt(xc + yc + zc);
+    return result;
+}
+double res = Distance(xa, ya, za, xb, yb, zb);
+double resRound = Math.Round(res, 2, MidpointRounding.ToZero);
+Console.WriteLine(resRound);
