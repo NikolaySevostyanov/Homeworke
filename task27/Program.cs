03a623c566f2b4de723c81c85ace300aef5ccb0c
Nikolay_S <sandaynsk@yandex.ru>
2022-11-28 20:25:54 +0700

    Выполнение домашнего задания к пятому семинару по C#

---
+++ task27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    int summa = 0;
+    while(num > 0)
+    {
+        summa += num % 10;
+        num /= 10;
+    }
+    return summa;
+}
+
+if (number < 0) number = - number;
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма цифр числа {number} = {sumNumbers}");
---
+++ task29/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 500);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArray(array);
+
+PrintArray(array);
